f872396895f72dd31f39f3f79bce09721d7c520b
seno0010 <mayamaya521999@gmail.com>
2024-01-30 05:04:01 +0900

    一旦コミットスポーンの高さ変更できていない

--- Assets/MyApp/Script/PlaneCheck.cs
+++ Assets/MyApp/Script/PlaneCheck.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
 
 public class PlaneCheck : MonoBehaviour
 {
@@ -14,37 +13,64 @@ public class PlaneCheck : MonoBehaviour
     [SerializeField]
     private GameObject _towerObj;
 
-    private List<GameObject>_towerlist = new();
+    private List<GameObject> _towerlist = new();
 
     [SerializeField]
     private Camera _smartCamera;
 
     private bool _setUp = true;
     private bool _gameMain = false;
-    private int  _towerCount = 0;
+    private int _towerCount = 0;
 
     private GameObject _stageCash;
     private float _spawnY = 0.5f;
 
-    private new Rigidbody _rigidbody;
+    private Rigidbody _rigidbody;
+
+    bool _stopPobj = true;
+
+    private float _waitTime = 1f; // 待機時間
+
+    private float _waitStartTime = 0f;
+    private bool _isWaiting = false;
+    private bool _firstloop = true;
+    private int _yPosition;
     // Start is called before the first frame update
     void Start()
     {
-      
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        StageSet();
-        GamePlay();
+        if (_setUp)
+        {
+            StageSet();
+        }
+        else if (_gameMain)
+        {
+            GamePlay();
+        }
     }
 
+
+    /// <summary>
+    /// ゲームのメイン処理になる部分
+    /// </summary>
     private void GamePlay()
     {
-         Vector3 _stagePosition = _stageCash.transform.position;
-         Vector3  _newSpawPosition = new Vector3(_stagePosition.x,_stagePosition.y + _spawnY ,_stagePosition.z);
-        if (Input.touchCount > 0 && _gameMain == true)
+        Vector3 _stagePosition = _stageCash.transform.position;
+        Vector3 _newSpawPosition = new(_stagePosition.x, _stagePosition.y + _spawnY+_yPosition, _stagePosition.z);
+        GameObject _gameObject = new();
+        if (_stopPobj)
+        {
+            _gameObject = Instantiate(_towerObj, _newSpawPosition, Quaternion.identity);
+            _towerlist.Add(_gameObject);
+            _rigidbody = _gameObject.GetComponent<Rigidbody>();
+            _stopPobj = false;
+        }
+        if (Input.touchCount > 0 && _gameMain)
         {
             Touch touch = Input.touches[0];
             if (touch.phase == TouchPhase.Began)
@@ -52,10 +78,7 @@ public class PlaneCheck : MonoBehaviour
                 var ray = Camera.main.ScreenPointToRay(touch.position);
                 if (Physics.Raycast(ray, out _hit))
                 {
-                    GameObject newgame = Instantiate(_towerObj ,_newSpawPosition,Quaternion.identity);
-                    _rigidbody = newgame.GetComponent<Rigidbody>();
-                    _towerlist.Add(newgame);
-                    Debug.Log("StartGame");
+                  
                 }
             }
 
@@ -63,34 +86,97 @@ public class PlaneCheck : MonoBehaviour
             {
                 Vector3 _vec3MovePosition = _smartCamera.transform.TransformDirection(new Vector3(touch.deltaPosition.x / 1000, 0, 0));
                 _towerlist[_towerCount].transform.localPosition += _vec3MovePosition;
+
             }
 
-            if(touch.phase == TouchPhase.Ended)
+            if (touch.phase == TouchPhase.Ended && _gameMain)
             {
                 _rigidbody.useGravity = true;
-                _towerCount++;
+                _towerCount++;               
+              //  Debug.Log("StartGame");
+                _isWaiting = true;
+                _waitStartTime = Time.time;
+                _firstloop = false;
             }
+            if (_isWaiting)
+            {
+                float elapsedTime = Time.time - _waitStartTime;
 
+                if (elapsedTime >= _waitTime)
+                {
+                 //   Debug.Log("StartGame" + _waitTime + " seconds.");
+                    _isWaiting = false;
+                    _stopPobj = true;
+                }
+            }
+            if (!_firstloop && _stopPobj)
+            {
+                var maxY = (_towerlist.Count > 0) ? _towerlist.Max(a => Mathf.Abs(a.transform.localPosition.y)) : float.MinValue;
+                _yPosition = Mathf.CeilToInt(maxY);
+                Debug.Log("StartGame" + maxY);
+            }
         }
     }
 
+    /// <summary>
+    /// ゲームをするためのステージの設置場所を決める
+    /// </summary>
     private void StageSet()
-    {
-        if(Input.touchCount > 0  && _setUp == true)
+    {    
+        if (Input.touchCount > 0 && _setUp)
         {
             Touch touch = Input.touches[0];
-            if(touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began)
             {
                 var ray = Camera.main.ScreenPointToRay(touch.position);
-                if(Physics.Raycast(ray, out _hit))
+                if (Physics.Raycast(ray, out _hit))
                 {
-                  _stageCash = Instantiate(_stage,_hit.transform.position, _hit.transform.rotation);
+                    _stageCash = Instantiate(_stage, _hit.transform.position, _hit.transform.rotation);
                 }
-                Debug.Log("Start");
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 _Vec3StagePosition = _smartCamera.transform.TransformDirection(new Vector3(touch.deltaPosition.x / 1000,0,0));
+                _stageCash.transform.localPosition += _Vec3StagePosition;
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+
+             //   Debug.Log("StartGameStage");
                 _gameMain = true;
                 _setUp = false;
             }
+            
         }
-      
+    }
+
+    /// <summary>
+    /// 指定時間待つ
+    /// </summary>
+    /// <param name="_waitTime">待ちたい時間</param>
+    private IEnumerator WaitTime(float _waitTime)
+    {
+        yield return new WaitForSeconds(_waitTime);
+    }
+
+
+    /// <summary>
+    /// リスト内にあるオブジェクト中で一番強いベロシティを返す
+    /// </summary>
+    /// <returns></returns>
+    private float GetMaxVelocity()
+    {
+        float _magnitude = 0f;
+        foreach(GameObject _veloList in _towerlist)
+        {
+            var r = _veloList.GetComponent<Rigidbody>();
+            var m = r.velocity.magnitude;
+            if(_magnitude < m)
+            {
+                _magnitude = m;
+            }
+        }
+        return _magnitude;
     }
 }
